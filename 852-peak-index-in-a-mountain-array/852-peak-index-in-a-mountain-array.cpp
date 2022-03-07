d609aa62ce05680e7a622af0a34bd589eff179b1
Aditya Raj Sahoo <66211350+adityarajsahoo@users.noreply.github.com>
2022-03-07 23:57:14 +0530

    Time: 23 ms (12.88%), Space: 11.5 MB (89.96%) - LeetHub

--- 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.cpp
+++ 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.cpp
@@ -5,7 +5,7 @@ public:
         
         // simple binary search is the solution
         // Peak exists somewhere in between start and end
-        int s = 1, e = n-2;
+        int s = 1, e = n-2; // if I keep s = 0 and e = n-1, I get an error
         while(s<=e){
             int mid = s + (e - s)/2;
             if(arr[mid] > arr[mid-1] && arr[mid] > arr[mid+1]) return mid;
