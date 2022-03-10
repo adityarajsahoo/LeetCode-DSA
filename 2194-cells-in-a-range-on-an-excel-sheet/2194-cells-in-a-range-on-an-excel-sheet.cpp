943c2b925abad4a99650fa278ec4989d8b898aae
Aditya Raj Sahoo <66211350+adityarajsahoo@users.noreply.github.com>
2022-03-10 23:59:28 +0530

    Time: 8 ms (25.34%), Space: 7.8 MB (68.02%) - LeetHub

---
+++ 2194-cells-in-a-range-on-an-excel-sheet/2194-cells-in-a-range-on-an-excel-sheet.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<string> cellsInRange(string s) {
+        vector<string>res;
+        int n=max(s[1]-'0', s[4]-'0'); // max column or row
+        int m=min(s[1]-'0', s[4]-'0'); // 1 column or row
+        
+        for(char c=s[0];c<=s[3];c++)
+        {
+            for(int i=m;i<=n;i++)
+            {
+                string t=c+to_string(i); // merges char and int
+                res.push_back(t);
+            }
+        }
+        return res;
+    }
+};
