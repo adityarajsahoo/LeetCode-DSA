9c3b23cbd1c9b24f9366d51174458c6186f5d4e9
Aditya Raj Sahoo <66211350+adityarajsahoo@users.noreply.github.com>
2022-03-11 23:59:40 +0530

    Time: 0 ms (100.00%), Space: 6 MB (96.72%) - LeetHub

--- 709-to-lower-case/709-to-lower-case.cpp
+++ 709-to-lower-case/709-to-lower-case.cpp
@@ -6,5 +6,6 @@ public:
             ch = tolower(ch);
         }
         return s;
+        //auto keyword specifies that the type of the variable that is begin declared will automatically be deduced from its initializer and for functions if their return type is auto then that will be evaluated by return type expression at runtime.
     }
 };
