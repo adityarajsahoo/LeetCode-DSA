680d7b3415babe45182aa5d3ebf29e3b241ddcee
Aditya Raj Sahoo <66211350+adityarajsahoo@users.noreply.github.com>
2022-08-13 23:32:39 +0530

    Time: 1018 ms (31.98%), Space: 339.3 MB (17.57%) - LeetHub

---
+++ 30-substring-with-concatenation-of-all-words/30-substring-with-concatenation-of-all-words.cpp
@@ -0,0 +1,38 @@
+// https://leetcode.com/problems/substring-with-concatenation-of-all-words/discuss/2418173/C%2B%2B-Unordered_map-and-Sliding-Window-or-Simple-and-easy-to-understand
+class Solution {
+public:
+    bool checkSubstring(unordered_map<string, int> wordCount, string s, int wordLen) {
+        for(int j=0; j<s.size(); j+=wordLen) {
+            string w = s.substr(j, wordLen);
+            if(wordCount.find(w) != wordCount.end()) {
+                if(--wordCount[w] == -1) {
+                    return false;
+                }
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    vector<int> findSubstring(string s, vector<string>& words) {
+        vector<int> res;
+        int wordLen = words[0].size();
+        int sLen = s.size();
+        int wordsWindow = words.size() * wordLen;
+        
+        unordered_map<string, int> wordCount;
+        for(int i=0; i<words.size(); i++) {
+            wordCount[words[i]]++;
+        }
+        
+        int i = 0;
+        while(i + wordsWindow <= sLen) {
+            if(checkSubstring(wordCount, s.substr(i, wordsWindow), wordLen)) {
+                res.push_back(i);
+            }
+            i++;
+        }
+        return res;
+    }
+};
