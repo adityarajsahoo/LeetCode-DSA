3f10db3bc26b24a759b0f3db5b9afa41d5a29d09
Aditya Raj Sahoo <66211350+adityarajsahoo@users.noreply.github.com>
2022-08-18 22:42:24 +0530

    Time: 286 ms (70.22%), Space: 78.3 MB (79.94%) - LeetHub

---
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.cpp
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/reduce-array-size-to-the-half/discuss/1319470/C%2B%2B-Simple-and-Clean-Solution-Explained-Easy-to-Understand
+class Solution {
+public:
+    int minSetSize(vector<int>& arr) {
+        unordered_map<int, int> counter;
+        priority_queue<int> q;
+        int res = 0, removed = 0;
+        
+        for (auto a : arr) counter[a]++;
+        for (auto c : counter) q.push(c.second);
+        
+        while (removed < arr.size() / 2) {
+            removed += q.top();
+            q.pop();
+            res++;
+        }
+        
+        return res;
+    }
+};
