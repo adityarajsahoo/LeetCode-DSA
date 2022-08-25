7266a935607f213b92b2633ba9dddfc72ce6f6bb
Aditya Raj Sahoo <66211350+adityarajsahoo@users.noreply.github.com>
2022-08-25 22:56:27 +0530

    Time: 26 ms (52.04%), Space: 8.8 MB (39.49%) - LeetHub

---
+++ 383-ransom-note/383-ransom-note.cpp
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/ransom-note/discuss/2476301/C%2B%2B-or-Hashmap-or-Very-Easy-Code
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        vector<int> a(26,0);
+        vector<int> b(26,0);
+        for(auto x:magazine)// freq of each char in magazine
+            b[x-'a']++;
+        for(auto x:ransomNote)// freq of each char in ransomNote
+            a[x-'a']++;
+        
+        for(int i=0;i<26;i++)
+        {
+		// ransomNote has freq of character (i+'a') greater than in magazine soo we can't construct it. ( False )
+            if(a[i]>b[i])
+                return false;
+        }
+		// for all character we had freq in ransomNote <= freq in magazine, soo it can be constructed. ( True )
+        return true;
+    }
+};
